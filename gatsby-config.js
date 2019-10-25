bd96928c469c98e33d52385a6f25eb240a4074a9
Israel Ramirez <israplenitud@gmail.com>
2019-10-25 16:38:09 -0500

    Adding Host process env

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,65 +2,67 @@ const contentful = require('contentful');
 const manifestConfig = require('./manifest-config');
 require('dotenv').config();
 
-const { ACCESS_TOKEN, SPACE_ID, ANALYTICS_ID, DETERMINISTIC } = process.env;
+const { ACCESS_TOKEN, SPACE_ID, HOST, ANALYTICS_ID, DETERMINISTIC } = process.env;
 
 const client = contentful.createClient({
-  space: SPACE_ID,
-  accessToken: ACCESS_TOKEN,
+    space: SPACE_ID,
+    accessToken: ACCESS_TOKEN,
+    host: HOST,
 });
 
 const getAboutEntry = entry => entry.sys.contentType.sys.id === 'about';
 
 const plugins = [
-  'gatsby-plugin-react-helmet',
-  {
-    resolve: 'gatsby-plugin-web-font-loader',
-    options: {
-      google: {
-        families: ['Cabin', 'Open Sans'],
-      },
+    'gatsby-plugin-react-helmet',
+    {
+        resolve: 'gatsby-plugin-web-font-loader',
+        options: {
+            google: {
+                families: ['Cabin', 'Open Sans'],
+            },
+        },
     },
-  },
-  {
-    resolve: 'gatsby-plugin-manifest',
-    options: manifestConfig,
-  },
-  'gatsby-plugin-styled-components',
-  {
-    resolve: 'gatsby-source-contentful',
-    options: {
-      spaceId: SPACE_ID,
-      accessToken: ACCESS_TOKEN,
+    {
+        resolve: 'gatsby-plugin-manifest',
+        options: manifestConfig,
     },
-  },
-  'gatsby-transformer-remark',
-  'gatsby-plugin-offline',
+    'gatsby-plugin-styled-components',
+    {
+        resolve: 'gatsby-source-contentful',
+        options: {
+            spaceId: SPACE_ID,
+            accessToken: ACCESS_TOKEN,
+            host: HOST,
+        },
+    },
+    'gatsby-transformer-remark',
+    'gatsby-plugin-offline',
 ];
 
 module.exports = client.getEntries().then(entries => {
-  const { mediumUser } = entries.items.find(getAboutEntry).fields;
+    const { mediumUser } = entries.items.find(getAboutEntry).fields;
 
-  plugins.push({
-    resolve: 'gatsby-source-medium',
-    options: {
-      username: mediumUser || '@medium',
-    },
-  });
-
-  if (ANALYTICS_ID) {
     plugins.push({
-      resolve: 'gatsby-plugin-google-analytics',
-      options: {
-        trackingId: ANALYTICS_ID,
-      },
+        resolve: 'gatsby-source-medium',
+        options: {
+            username: mediumUser || '@medium',
+        },
     });
-  }
 
-  return {
-    siteMetadata: {
-      isMediumUserDefined: !!mediumUser,
-      deterministicBehaviour: !!DETERMINISTIC,
-    },
-    plugins,
-  };
+    if (ANALYTICS_ID) {
+        plugins.push({
+            resolve: 'gatsby-plugin-google-analytics',
+            options: {
+                trackingId: ANALYTICS_ID,
+            },
+        });
+    }
+
+    return {
+        siteMetadata: {
+            isMediumUserDefined: !!mediumUser,
+            deterministicBehaviour: !!DETERMINISTIC,
+        },
+        plugins,
+    };
 });
